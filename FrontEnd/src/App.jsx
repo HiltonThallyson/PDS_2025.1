c00134501f83de573e0e2ed153b4b79fc31f595a
Adriel Costa <adryeltc2001@gmail.com>
2025-04-17 16:57:03 -0300

    remove importação de arquivos css antigos em App.jsx e main.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,8 @@
-// src/App.jsx
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'; // Import Routes and Route
-import LoginPage from './pages/LoginPage';         // Import your page components
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
-// Removed the direct import of './style.css' as it's now in main.jsx
 
 function App() {
   return (
