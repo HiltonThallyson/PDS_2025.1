5f2a89089215a25d41c74fbab62d16d64b04db78
Joelson Silva <joelsonsilvahlr@gmail.com>
2025-04-18 01:09:03 -0300

    Criação dos Pacotes de a aplcaçao criação dda funcionalidade: buscar por titulo na api googlleBooks

---
+++ BackEnd/src/main/java/br/imd/mybookplace/controllers/LivroController.java
@@ -0,0 +1,27 @@
+package br.imd.mybookplace.controllers;
+
+
+import br.imd.mybookplace.DTOS.LivroDTO;
+import br.imd.mybookplace.services.GoogleBookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/livros")
+public class LivroController {
+    public final GoogleBookService googleService;
+
+
+    public LivroController(GoogleBookService googleService) {
+        this.googleService = googleService;
+    }
+
+    @GetMapping("/titulo")
+    public LivroDTO buscarLivroPorTitulo(@RequestParam String titulo){
+        System.out.println(titulo);
+        return googleService.buscarLivroPorTitulo(titulo);
+    }
+
+}
