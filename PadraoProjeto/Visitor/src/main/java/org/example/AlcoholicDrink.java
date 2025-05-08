50a9642d2003aa8d1b4e7ba0f9018a5e74c07209
Adriel Costa <adryeltc2001@gmail.com>
2025-05-08 16:09:23 -0300

    Modificacoes Finais nos Padroes de Projeto

--- PadraoProjeto/Visitor/src/main/java/org/example/AlcoholicDrink.java
+++ PadraoProjeto/Visitor/src/main/java/org/example/AlcoholicDrink.java
@@ -1,12 +1,29 @@
 package org.example;
 
-public class AlcoholicDrink extends VisitableProduct{
-    public AlcoholicDrink(String name , float price) {
-        super(name, price);
+public class AlcoholicDrink implements VisitableProduct{
+    private String name;
+    private float price;
+
+    public AlcoholicDrink(String name, float price) {
+        this.name = name;
+        this.price = price;
     }
 
     @Override
     public double getPriceWithTax(TaxVisitor taxVisitor) {
         return taxVisitor.calculateTaxesForAlcoholicDrink(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(float price) {
+        this.price = price;
     }
 }
--- PadraoProjeto/Visitor/src/main/java/org/example/VisitableProduct.java
+++ PadraoProjeto/Visitor/src/main/java/org/example/VisitableProduct.java
@@ -1,25 +1,6 @@
 package org.example;
 
-abstract class VisitableProduct {
-    private String name;
-    private double price;
-
-    VisitableProduct(String name, double price) {
-        this.name = name;
-        this.price = price;
-    }
+interface VisitableProduct {
     abstract double getPriceWithTax(TaxVisitor taxVisitor);
-    
-    public String getName() {
-        return name;
-    }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+    abstract double getPrice();
 }
