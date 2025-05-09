c858c1c8ddebb4b9354071a33708ed188fd9cc7b
HiltonThallyson <hilton.machado.088@ufrn.edu.br>
2025-05-09 11:25:43 -0300

    #18[Feat]-Adicionando mudança no agente, estruturando resposta iniciada

--- Agent/agent/google_agent.py
+++ Agent/agent/google_agent.py
@@ -1,8 +1,11 @@
 import os
 from dotenv import load_dotenv
-from langchain.chat_models import init_chat_model
+# from langchain.chat_models import init_chat_model
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
-from langgraph.prebuilt import create_react_agent
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.agents import AgentExecutor
+# from langgraph.prebuilt import create_react_agent
 
 # Carrega variáveis de ambiente
 load_dotenv()
@@ -17,12 +20,80 @@ if not tavily_key or not google_key:
 os.environ["TAVILY_API_KEY"] = tavily_key
 os.environ["GOOGLE_API_KEY"] = google_key
 
+json_schema = {
+    "title": "book title",
+    "author": "book author",
+    "price" : "book price",
+    "link" : "store link to book",
+    "type": "object",
+    "properties": {
+        "title": {
+            "type": "string",
+            "description": "The title of the book",
+        },
+        "autor": {
+            "type": "string",
+            "description": "The author to the book",
+        },
+        "price": {
+            "type": "double",
+            "description": "The price of the book",
+            "default": None,
+        },
+        "link": {
+            "type": "string",
+            "description": "The link to the store",
+            "default": None,
+        },
+    },
+    "required": ["title", "price", "link"],
+}
+
 # Inicializa o modelo e a ferramenta
-llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
 tavily_tool = TavilySearch(max_results=5, topic="general")
 
-# Cria o agente
-agent = create_react_agent(llm, [tavily_tool])
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant that search for prices of selected books in the internet and return the information to the user."
+            ,
+        ),
+        ("human", "{input}"),
+    ]
+)
 
-def stream_agent(messages: str):
-    return agent.stream({"messages": messages}, stream_mode="values")
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2)
+
+llm.with_structured_output(schema=json_schema)
+llm.bind_tools([tavily_tool])
+
+agent = (prompt | llm)
+
+agent_executor = AgentExecutor(agent=agent, tools=[tavily_tool], verbose=True)
+
+def get_agent():
+    return agent_executor
+
+
+
+
+
+
+
+
+# Cria o agente
+
+# agent = create_react_agent(
+#     llm,
+#     [tavily_tool],
+# )
+
+
+def get_agent():
+    return agent
--- Agent/controller/question_controller.py
+++ Agent/controller/question_controller.py
@@ -1,18 +1,20 @@
 from fastapi import APIRouter
-from agent.google_agent import stream_agent
+from agent.google_agent import get_agent
+
 from model.query import Query 
 
 
 router = APIRouter()
 
+
 @router.post("/ask/")
 async def ask_question(query: Query):
+    
     try:
-        steps = stream_agent([{"role": "user", "content": query.prompt}]
-)
-        final_message = ""
-        for step in steps:
-            final_message = step["messages"][-1].content
+        agent = get_agent()
+    
+        final_message = agent.invoke({"input": query.prompt})
+        
         return {"answer": final_message}
     except Exception as e:
         return {"error": str(e)}
