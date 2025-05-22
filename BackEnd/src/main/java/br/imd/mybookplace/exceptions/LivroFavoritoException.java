674ecac729907270105aa51e57c41b1f1ecd55e4
Adriel Costa <adryeltc2001@gmail.com>
2025-05-22 14:59:34 -0300

    #33 [Feat] - Adiciona exceção LivroFavoritoException

---
+++ BackEnd/src/main/java/br/imd/mybookplace/exceptions/LivroFavoritoException.java
@@ -0,0 +1,8 @@
+package br.imd.mybookplace.exceptions;
+
+public class LivroFavoritoException extends RuntimeException{
+    public LivroFavoritoException(String message){
+        super (message);
+    }
+    
+}
