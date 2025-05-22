a34202e162f7ae6b507ce6bc26f16170e9825826
Adriel Costa <adryeltc2001@gmail.com>
2025-05-22 11:03:11 -0300

    #33 [Feat] - adiciona handler global para tratamento de exceções

---
+++ BackEnd/src/main/java/br/imd/mybookplace/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package br.imd.mybookplace.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Erro interno do servidor. Por favor, tente novamente mais tarde");
+    }
+
+    @ExceptionHandler(GoogleBooksApiException.class)
+    public ResponseEntity<String> handleGoogleBooksApiException(GoogleBooksApiException e){
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
+                .body("Não foi possível obter informações dos livros no momento. Por favor, tente novamente mais tarde.");
+    }
+
+}
