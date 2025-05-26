4903f905ee59ea13ba4196ed30aa0f8df6b16635
Adriel Costa <adryeltc2001@gmail.com>
2025-05-26 15:50:55 -0300

    #33 [Refactor] - Adiciona um construtor com todos os parâmetros a LivroFavoritoException

--- BackEnd/src/main/java/br/imd/mybookplace/exceptions/LivroFavoritoException.java
+++ BackEnd/src/main/java/br/imd/mybookplace/exceptions/LivroFavoritoException.java
@@ -4,5 +4,9 @@ public class LivroFavoritoException extends RuntimeException{
     public LivroFavoritoException(String message){
         super (message);
     }
+
+    public LivroFavoritoException(String message, Throwable cause){
+        super (message, cause);
+    }
     
 }
