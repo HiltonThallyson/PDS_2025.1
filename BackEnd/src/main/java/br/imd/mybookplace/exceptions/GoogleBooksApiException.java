ab74e66f75d147fdb7a33953851d6b5abc1f5d2b
Adriel Costa <adryeltc2001@gmail.com>
2025-05-26 15:51:15 -0300

    #33 [Refactor] - Adiciona um construtor a GoogleBooksApiException

--- BackEnd/src/main/java/br/imd/mybookplace/exceptions/GoogleBooksApiException.java
+++ BackEnd/src/main/java/br/imd/mybookplace/exceptions/GoogleBooksApiException.java
@@ -1,6 +1,10 @@
 package br.imd.mybookplace.exceptions;
 
 public class GoogleBooksApiException extends RuntimeException {
+    public GoogleBooksApiException(String message) {
+        super(message);
+    }
+    
     public GoogleBooksApiException(String message, Throwable cause) {
         super(message, cause);
     }
