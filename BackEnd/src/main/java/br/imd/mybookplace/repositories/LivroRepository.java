c50cb2179af399ac11dfbc28466c99d8412c315f
Adriel Costa <adryeltc2001@gmail.com>
2025-07-17 13:47:25 -0300

    Remove a pasta "Backend"

--- BackEnd/src/main/java/br/imd/mybookplace/repositories/LivroRepository.java
+++
@@ -1,8 +0,0 @@
-package br.imd.mybookplace.repositories;
-
-import br.imd.framework.repositories.ProdutoRepository;
-import br.imd.mybookplace.entities.Livro;
-
-public interface LivroRepository extends ProdutoRepository<Livro, Long>{
-    
-}
