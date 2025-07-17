94ffcd28398c077e32c7326dde59cda880ffa0c1
HiltonThallyson <hilton.machado.088@ufrn.edu.br>
2025-07-17 11:55:30 -0300

    #53-removendo pastas desnecessárias

--- PadraoProjeto/Visitor/src/main/java/org/example/TaxVisitor.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public interface TaxVisitor {
-    double calculateTaxesForFood(Food food);
-    double calculateTaxesForCigarette(Cigarette cigarette);
-    double calculateTaxesForAlcoholicDrink(AlcoholicDrink alcoholicDrink);
-}
