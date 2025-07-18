3e89ce616ac18725f8ac8c1e2e458cb6f4b63a33
HiltonThallyson <hilton.machado.088@ufrn.edu.br>
2025-07-18 15:59:51 -0300

    #53-versão final da aplicação de livros com as modificações no framework

--- BackEnd/mybookplaceapi/src/main/java/br/imd/mybookplace/DTOS/BookLLMRequestDTO.java
+++ BackEnd/mybookplaceapi/src/main/java/br/imd/mybookplace/DTOS/BookLLMRequestDTO.java
@@ -9,7 +9,11 @@ public class BookLLMRequestDTO extends LLMRequestDTO{
     }
 
     public String getPrompt() {
-        return super.getPrompt();
+        String promptConfiguration = "You are a shopping assistant who specializes in finding the best deals on books."+
+    "Your task is to parse the given prompt and return a list of relevant deals. Each deal should contain the book"+ 
+    "title, price, purchase link, and image URL for the book. You should return a list of JSON objects,"+ 
+    "where each object represents a deal. Organize them as follows: title, author, price, link, imageUrl. prompt: " + super.getPrompt();
+        return promptConfiguration;
     }
 
 }
